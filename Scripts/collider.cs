66b34f78f4425ffe64a8d7fb680e9a47c1cf333e
Pagano Ennio <e.pagano@etec.local>
2023-05-04 12:49:26 -0300

    Assets botones

---
+++ Scripts/collider.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class colider : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log(collision.gameObject.name);
+    }
+
+
+}
