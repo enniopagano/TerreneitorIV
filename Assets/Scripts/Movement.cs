06b2560df740c5f2fa4d77ae409c01d1ebc176d1
Enzo-Querini <e.querini@alumno.etec.um.edu.ar>
2023-05-11 11:30:12 -0300

    movement_fix_1

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,15 +5,15 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     public void force_up(float force, Rigidbody2D rigid){
-       rigid.AddForce(0,force,0,ForceMode.Impulse);
+       rigid.AddForce(new Vector2(0,force),ForceMode2D.Impulse);
     }
     public void force_left(float force, Rigidbody2D rigid){
-        rigid.AddForce(force,0,0,ForceMode.Impulse);
+        rigid.AddForce(new Vector2(force,0),ForceMode2D.Impulse);
     }
     public void force_down(float force, Rigidbody2D rigid){
-        rigid.AddForce(0,-force,0,ForceMode.Impulse);
+        rigid.AddForce(new Vector2(0,-force),ForceMode2D.Impulse);
     }
     public void force_right(float force, Rigidbody2D rigid){
-        rigid.Addforce(-force,0,0,Forcemode.Impulse);
+        rigid.AddForce(new Vector2(force,0),ForceMode2D.Impulse);
     }
 }
