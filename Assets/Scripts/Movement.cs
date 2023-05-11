3513fd422fd9da9304c4e7b22469c7a6660795c3
Enzo-Querini <e.querini@alumno.etec.um.edu.ar>
2023-05-11 10:09:00 -0300

    movement

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,29 +4,16 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    Rigidbody rigid;
-    public float thrust = 1f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        rigid = Getcomponent<Rigidbody>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    public void force_up(float force){
+    public void force_up(float force, Rigidbody2D rigid){
        rigid.AddForce(0,force,0,ForceMode.Impulse);
     }
-    public void force_left(float force){
+    public void force_left(float force, Rigidbody2D rigid){
         rigid.AddForce(force,0,0,ForceMode.Impulse);
     }
-    public void force_down(float force){
+    public void force_down(float force, Rigidbody2D rigid){
         rigid.AddForce(0,-force,0,ForceMode.Impulse);
     }
-    public void force_right(float force){
+    public void force_right(float force, Rigidbody2D rigid){
         rigid.Addforce(-force,0,0,Forcemode.Impulse);
     }
 }
