73181082925218aab037b55b0f9397a2b0b188de
root <root@mateo.mateo>
2023-06-22 19:27:54 -0300

    Audio slider funcionando

--- Assets/SoundManager.cs
+++ Assets/SoundManager.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class SoundManager : MonoBehaviour
 {
     [SerializeField] Slider volumeSlider;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -18,11 +19,11 @@ public class SoundManager : MonoBehaviour
         
     }
     public void CambiarVolumen(){
-        Audiolistener.volume = volumeSlider.value;
+        AudioListener.volume = volumeSlider.value;
         GuardarVolumen();
     }
     private void CargarVolumen(){
-        volumeSlider.volume = PlayerPrefs.GetFloat("musicVolume");
+        volumeSlider.value = PlayerPrefs.GetFloat("musicVolume");
     }
     private void GuardarVolumen(){
         PlayerPrefs.SetFloat("musicVolume",volumeSlider.value);
