f9f107d7ff57b96611e36d444c6ee15b503dc1e4
root <root@mateo.mateo>
2023-06-24 23:20:24 -0300

    ScriptableObject con sprite de enemigo

--- Assets/Enemigo.cs
+++ Assets/Enemigo.cs
@@ -2,17 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.UI;
 public class Enemigo : MonoBehaviour
 {
+    public Mounstro mounstro;
+    public SpriteRenderer spriteRenderer;
+   
     void Awake(){
 
     }
+    public Image imagen;
     float vida;
-
+    int min = 1;
+    int max = 3;
+    int numeroAleatorio;
    void Start(){
     vida = 100;
+    numeroAleatorio = Random.Range(min,max);
+    spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+    Debug.Log("salio el numero: "+numeroAleatorio);
+    if (numeroAleatorio == 1)
+    {
+        spriteRenderer.sprite = mounstro.imagen1;
+        Debug.Log("LO cambio a imagen1");
     }
+    if(numeroAleatorio == 2){
+        spriteRenderer.sprite = mounstro.imagen2;
+        Debug.Log("Lo cambio a imagen2");
+    }
+        }
     public void TomarDaño(float daño){
         vida -= daño;
         if (vida <= 0)
---
+++ Assets/Scripts/Mounstro.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "New Monstruo", menuName = "Mounstro")]
+public class Mounstro:ScriptableObject{
+    public Sprite imagen1;
+    public Sprite imagen2;
+
+
+}
